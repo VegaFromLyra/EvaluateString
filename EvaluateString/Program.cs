14da6c47ec660187de8f74959d4056d4f4f7057d
VegaFromLyra <asha.ab@gmail.com>
2014-03-19 22:22:40 -0700

    Simplified the code

--- EvaluateString/Program.cs
+++ EvaluateString/Program.cs
@@ -18,8 +18,10 @@ namespace EvaluateString
         {
             try
             {
-                string expression  = "3+4*8/2-1";
-                Console.WriteLine("Result of evaluating {0} is {1}", expression, Evaluate(expression));
+                Test("3 + 4 * 8 / 2 - 1");
+                Test("12 * 8 - 8 + 8");
+                Test("12 * 8 + 11 * 11");
+                Test("2 + 7 - 3 * 15 / 3 * 6");
             }
             catch (Exception e)
             {
@@ -27,83 +29,98 @@ namespace EvaluateString
             }
         }
 
+        static void Test(string expression)
+        {
+            Console.WriteLine("Result of evaluating {0} is {1}", expression, Evaluate(expression));
+        }
+
         static int Evaluate(string expression)
         {
             Stack<int> operandStack = new Stack<int>();
-            Stack<char> operatorStack = new Stack<char>();
+            Stack<string> operatorStack = new Stack<string>();
 
             int current = 0;
 
             int number = 0;
 
-            while (current < expression.Length)
+            string[] tokens = expression.Split(' ');
+
+            while (current < tokens.Length)
             {
-                if (IsNumber(expression[current], out number))
+                if (IsNumber(tokens[current], out number))
                 {
                     operandStack.Push(number);
-                    current++;
-                }
-                else if (expression[current] == '*' || expression[current] == '/')
-                {
-                    if (current + 1 < expression.Length)
-                    {
-                        int nextNumber = 0;
-                        if (IsNumber(expression[current + 1], out nextNumber))
-                        {
-                            int top = operandStack.Pop();
-
-                            if (expression[current] == '*')
-                            {
-                                operandStack.Push(top * nextNumber);
-                            }
-                            else
-                            {
-                                operandStack.Push(top / nextNumber);
-                            }
-
-                            current = current + 2;
-                        }
-                        else
-                        {
-                            throw new Exception("Invalid argument");
-                        }
-                    }
-                    else
-                    {
-                        throw new ArgumentException("Invalid input");
-                    }
                 }
                 else
                 {
-                    operatorStack.Push(expression[current]);
-                    current++;
+                    while (operatorStack.Count > 0 && 
+                        hasHigherPrecedence(operatorStack.Peek(), tokens[current]))
+                    {
+                        int operand2 = operandStack.Pop();
+                        int operand1 = operandStack.Pop();
+                        operandStack.Push(Operate(operand1, operand2, operatorStack.Pop()));
+                    }
+
+                    operatorStack.Push(tokens[current]);
                 }
+
+                current++;
             }
 
-            // Now handle the + and - operations
+            // Now handle the remaidner operations
             while (operatorStack.Count > 0)
             {
-                char oper = operatorStack.Pop();
+                string oper = operatorStack.Pop();
 
                 int operand2 = operandStack.Pop();
                 int operand1 = operandStack.Pop();
 
-                if (oper == '+')
-                {
-                    operandStack.Push(operand1 + operand2);
-                }
-                else if (oper == '-')
-                {
-                    operandStack.Push(operand1 - operand2);
-                }
+                operandStack.Push(Operate(operand1, operand2, oper));
             }
 
             return operandStack.Peek();
         }
 
-        static bool IsNumber(char digit, out int number)
+        static bool IsNumber(string digit, out int number)
         {
-            return Int32.TryParse(digit.ToString(), out number);
+            return Int32.TryParse(digit, out number);
+        }
+
+        static int Operate(int operand1, int operand2, string oper)
+        {
+            if (oper.Equals("+"))
+            {
+                return operand1 + operand2;
+            }
+            else if (oper.Equals("-"))
+            {
+                return operand1 - operand2;
+            }
+            else if (oper.Equals("*"))
+            {
+                return operand1 * operand2;
+            }
+            else if (oper.Equals("/"))
+            {
+                return operand1 / operand2;
+            }
+            else
+            {
+                throw new Exception("Invalid operator");
+            }
+        }
+
+        // Returns true if s1 has higher or equal 
+        // precedence as s2
+        static bool hasHigherPrecedence(string s1, string s2)
+        {
+            if ((s1.Equals("+") || s1.Equals("-")) &&
+               (s2.Equals("*") || s2.Equals("/")))
+            {
+                return false;
+            }
+
+            return true;
         }
     }
 }
